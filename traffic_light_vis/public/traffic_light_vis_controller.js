336e5bf134e7eddefe01b6db3fd2ec2b5a1c614d
Benajmin Bondi <bondi@kaminario.com>
2016-03-09 13:40:37 +0200

    Cleaned up code by moving logic to controller from the html ui

--- traffic_light_vis/public/traffic_light_vis_controller.js
+++ traffic_light_vis/public/traffic_light_vis_controller.js
@@ -10,9 +10,15 @@ define(function (require) {
     $scope.processTableGroups = function (tableGroups) {
       tableGroups.tables.forEach(function (table) {
         table.columns.forEach(function (column, i) {
+            let metricValue = table.rows[0][i];
+            let params = $scope.vis.params;
           metrics.push({
             label: column.title,
-            value: table.rows[0][i]
+            value: metricValue,
+            isRedLightOn: (!params.invertScale && metricValue <= params.redThreshold) || (params.invertScale && metricValue.value >= params.redThreshold),
+            isYellowOn: (!params.invertScale && metricValue > params.redThreshold && metricValue < params.greenThreshold) || (params.invertScale && metricValue < params.redThreshold && metricValue > params.greenThreshold),
+            isGreenOn: (params.redAndGreenOnly && ((!params.invertScale && metricValue > params.redThreshold) || (params.invertScale && metricValue < params.redThreshold))) || 
+                 (!params.redAndGreenOnly && ((!params.invertScale && metricValue >= params.greenThreshold) || (params.invertScale && metricValue <= params.greenThreshold)))
           });
         });
       });
